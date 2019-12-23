16d654bb144a87c4e6681966825320b71ff160b3
zipperr <zipperr@i.softbank.jp>
2019-12-23 10:06:15 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DOTFILES_PATH="$HOME/dotfiles"
+DOTFILES_PATH="$HOME/dotfilesTest"
 if [ -d $DOTFILES_PATH ]; then
-    echo "あああ"
+    echo "already exists"
 else
-    echo "fjaklsd"
+    echo "downloading"
     if type git > /dev/null 2>&1; then
         git clone https://github.com/zipperr/OneLineDotfiles
     else
         curl -sL https://github.com/zipperr/OneLineDotfiles/archive/master.tar.gz | tar xz
         mv dotfiles-master dotfilesTest
     fi
-    echo "ajskldf"
+    echo "success"
 fi
