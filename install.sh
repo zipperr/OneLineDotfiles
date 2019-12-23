74452e8dc97234e86bcf890d2586eb38df58719c
zipperr <zipperr@i.softbank.jp>
2019-12-23 10:01:26 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
-echo ${HOME}
-DOT_DIRECTORY="${HOME}/dotfilesTest"
-DOT_TARBALL="https://github.com/zipperr/OneLineDotfiles"
-REMOTE_URL="git@github.com:zipperr/OneLineDotfiles.git"
-# ディレクトリがなければダウンロード（と解凍）する
-if [ ! -d ${DOT_DIRECTORY} ]; then
-  echo "Downloading dotfiles..."
-  mkdir ${DOT_DIRECTORY}
-
-  if type "git" > /dev/null 2>&1; then
-    git clone --recursive "${REMOTE_URL}" "${DOT_DIRECTORY}"
-  else
-    curl -fsSLo ${HOME}/dotfiles.tar.gz ${DOT_TARBALL}
-    tar -zxf ${HOME}/dotfiles.tar.gz --strip-components 1 -C ${DOT_DIRECTORY}
-    rm -f ${HOME}/dotfiles.tar.gz
-  fi
-
-  echo $(tput setaf 2)Download dotfiles complete!. ✔︎$(tput sgr0)
+if [ -d $DOTFILES_PATH ]; then
+    echo "あああ"
+else
+    echo "fjaklsd"
+    if type git > /dev/null 2>&1; then
+        git clone https://github.com/zipperr/OneLineDotfiles
+    else
+        curl -sL https://github.com/zipperr/OneLineDotfiles/archive/master.tar.gz | tar xz
+        mv dotfiles-master dotfiles
+    fi
+    echo "ajskldf"
 fi
