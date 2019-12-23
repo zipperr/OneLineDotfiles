0ac4ce1419297420c9624445e1d53ef43b73a8b9
zipperr <zipperr@i.softbank.jp>
2019-12-23 13:33:24 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 download_dotfiles() {
+    echo 'Download Dotfiles'
     REPOSITORY_NAME="OneLineDotfiles"
     REPOSITORY_URL="https://github.com/zipperr/"$REPOSITORY_NAME
     DOTFILES_PATH="$HOME/$REPOSITORY_NAME"
@@ -21,6 +22,7 @@ download_dotfiles() {
 }
 
 setup(){
+    echo 'Setup for each OS'
     if [ "$(uname)" == "Darwin" ]; then
         echo 'mac'
     elif [ "$(expr substr $(uname -s) 1 5 > /dev/null 2>&1)" == "MINGW" ]; then
@@ -31,23 +33,24 @@ setup(){
 }
 
 create_symbolic_links(){
-        export MSYS=winsymlinks:nati
-        rm -f $HOME/.vimrc
-        rm -f $HOME/.bashrc
-        rm -f $HOME/.bash_completion
-        rm -f $HOME/.zshrc
-        rm -f $HOME/.git-prompt.sh
-        rm -f $HOME/.gitconfig
-        rm -f $HOME/.gitignore_global
-        rm -f $HOME/.minttyrc
-        ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-        ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
-        ln -s $HOME/dotfiles/.bash_completion $HOME/.bash_completion
-        ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
-        ln -s $HOME/dotfiles/.git-prompt.sh $HOME/.git-prompt.sh
-        ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-        ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
-        ln -s $HOME/dotfiles/.gitignore_global $HOME/.minttyrc
+    echo 'Create SymbolicLinks'
+    export MSYS=winsymlinks:nati
+    rm -f $HOME/.vimrc
+    rm -f $HOME/.bashrc
+    rm -f $HOME/.bash_completion
+    rm -f $HOME/.zshrc
+    rm -f $HOME/.git-prompt.sh
+    rm -f $HOME/.gitconfig
+    rm -f $HOME/.gitignore_global
+    rm -f $HOME/.minttyrc
+    ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+    ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
+    ln -s $HOME/dotfiles/.bash_completion $HOME/.bash_completion
+    ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+    ln -s $HOME/dotfiles/.git-prompt.sh $HOME/.git-prompt.sh
+    ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+    ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
+    ln -s $HOME/dotfiles/.gitignore_global $HOME/.minttyrc
 }
 
 download_dotfiles
