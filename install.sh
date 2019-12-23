62718da45955ee28cf89040c12814ef6493072b3
zipperr <zipperr@i.softbank.jp>
2019-12-23 13:26:01 +0900

    Update

--- install.sh
+++ install.sh
@@ -23,9 +23,9 @@ download_dotfiles() {
 setup(){
     if [ "$(uname)" == "Darwin" ]; then
         echo 'mac'
-    elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    elif [ "$(expr substr $(uname -s) 1 5 > /dev/null 2>&1)" == "MINGW" ]; then
         echo 'windows'
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    elif [ "$(expr substr $(uname -s) 1 5 > /dev/null 2>&1)" == "Linux" ]; then
         echo 'linux'
     fi
 }
@@ -50,14 +50,14 @@ create_symbolic_links(){
         ln -s $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
         ln -s $HOME/dotfiles/.gitignore_global $HOME/.minttyrc
     else
-        sudo rm $HOME/.vimrc
-        sudo rm $HOME/.bashrc
-        sudo rm $HOME/.bash_completion
-        sudo rm $HOME/.zshrc
-        sudo rm $HOME/.git-prompt.sh
-        sudo rm $HOME/.gitconfig
-        sudo rm $HOME/.gitignore_global
-        sudo rm $HOME/.minttyrc
+        rm -f $HOME/.vimrc
+        rm -f $HOME/.bashrc
+        rm -f $HOME/.bash_completion
+        rm -f $HOME/.zshrc
+        rm -f $HOME/.git-prompt.sh
+        rm -f $HOME/.gitconfig
+        rm -f $HOME/.gitignore_global
+        rm -f $HOME/.minttyrc
         ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
         ln -s $HOME/dotfiles/.bashrc $HOME/.bashrc
         ln -s $HOME/dotfiles/.bash_completion $HOME/.bash_completion
