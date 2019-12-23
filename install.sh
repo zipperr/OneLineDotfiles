4a4fb360d684621e92707b8eeca4a185caa35c77
zipperr <zipperr@i.softbank.jp>
2019-12-23 10:11:32 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 DOTFILES_PATH="$HOME/dotfilesTest"
+
 if [ -d $DOTFILES_PATH ]; then
     echo "already exists"
 else
     echo "downloading"
     if type git > /dev/null 2>&1; then
+
+        echo "using git"
         git clone https://github.com/zipperr/OneLineDotfiles
     else
+        echo "using curl"
         curl -sL https://github.com/zipperr/OneLineDotfiles/archive/master.tar.gz | tar xz
         mv dotfiles-master dotfilesTest
     fi
